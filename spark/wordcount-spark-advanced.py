e5fb76bede00ee9abe8b7029d73f3bd0fc155cd1
Eric Camalionte <eric80hz@yahoo.com.br>
2016-03-13 17:58:31 -0300

    Fix HDFS path

--- spark/wordcount-spark-advanced.py
+++ spark/wordcount-spark-advanced.py
@@ -2,7 +2,7 @@
 
 #Shows
 #> hdfs dfs -ls input/map-reduce
-show_views_file = sc.textFile("input/map-reduce/join2_gennum?.txt")
+show_views_file = sc.textFile("input/map-reduce/final/join2_gennum?.txt")
 show_views_file.take(2)
 
 # [u'Hourly_Sports,21', u'PostModern_Show,38']
@@ -15,7 +15,7 @@ show_views = show_views_file.map(split_show_views)
 
 
 #Channels
-show_channel_file = sc.textFile("input/map-reduce/join2_genchan?.txt")
+show_channel_file = sc.textFile("input/map-reduce/final/join2_genchan?.txt")
 def split_show_channel(line):
     show, channel = line.split(',')
     return (show, channel)
